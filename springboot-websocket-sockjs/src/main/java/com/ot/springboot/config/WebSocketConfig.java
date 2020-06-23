7bdc88aba422642cfd9948f3d0fc94d16adb7cc7
hello world <reallyalien@yeah.net>
2020-06-23 20:23:35 +0800

    setApplicationDestinationPrefixes

--- springboot-websocket-sockjs/src/main/java/com/ot/springboot/config/WebSocketConfig.java
+++ springboot-websocket-sockjs/src/main/java/com/ot/springboot/config/WebSocketConfig.java
@@ -15,11 +15,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
      * @param registry
      */
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/topicServer").withSockJS();
-        registry.addEndpoint("/queueServer").withSockJS();//注册两个STOMP的endpoint，分别用于广播和点对点
+        registry.addEndpoint("/topicServer").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/queueServer").setAllowedOrigins("*").withSockJS();//注册两个STOMP的endpoint，分别用于广播和点对点
     }
 
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic","/user");//topic用来广播，user用来实现queue
+        registry.setApplicationDestinationPrefixes("/app");//给@MessageMapping访问的时候需要加前缀
     }
 }
